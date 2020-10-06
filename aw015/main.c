3c4f2f6873ca27cd112e80609c0ee30ce55c7411
Nosikov <nosevg@yandex.ru>
2020-10-06 19:02:06 +0700

    WinAPI

---
+++ aw015/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _WIN32_WINNT 0x0A00
+#include <windows.h>
+
+int main()
+{
+    HWND hwnd = GetConsoleWindow();
+    HDC dc =GetDC (hwnd);
+
+    SelectObject (dc, GetStockObject(DC_BRUSH));
+    SetDCBrushColor (dc, RGB(255,0,255));
+    SetDCPenColor (dc, RGB(0,255,0));
+    Rectangle (dc,10,10,110,110);
+    Ellipse (dc,200,100,300,200);
+
+    return 0;
+}
